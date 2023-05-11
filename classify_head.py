b229ff02410adc2cf451eabb9836d9bf332d1093
Gzdang <rzlin579@gmail.com>
2023-05-11 03:10:13 +0000

    upload_ml_final_task

---
+++ classify_head.py
@@ -0,0 +1,297 @@
+import argparse
+import numpy as np
+import os
+import time
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from timm.models.layers import trunc_normal_
+from utils.lars import LARS
+
+import utils.misc as misc
+from utils.pos_embed import interpolate_pos_embed
+
+import models.models_vit as models_vit
+
+from utils import lr_sched
+from dataset.imbalance_cifar import ImbalanceCIFAR10
+from utils.semi import *
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
+    parser.add_argument('--batch_size', default=1024, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+
+    # Model parameters
+    parser.add_argument('--model', default='vit_cifar', type=str, metavar='MODEL',
+                        help='Name of model to train')
+
+    parser.add_argument('--input_size', default=32, type=int,
+                        help='images input size')
+
+    parser.add_argument('--mask_ratio', default=0.75, type=float,
+                        help='Masking ratio (percentage of removed patches).')
+
+    parser.add_argument('--norm_pix_loss', action='store_true',
+                        help='Use (per-patch) normalized pixels as targets for computing loss')
+    parser.set_defaults(norm_pix_loss=False)
+
+    # Optimizer parameters
+    parser.add_argument('--weight_decay', type=float, default=0.0,
+                        help='weight decay (default: 0.05)')
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=0.1, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=10, metavar='N',
+                        help='epochs to warmup LR')
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='./data', type=str,
+                        help='dataset path')
+
+    parser.add_argument('--output_dir', default='./log/classify_head',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='./classify_head',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='./output_dir/checkpoint-99.pth',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser
+
+
+def main(args):
+    misc.init_distributed_mode(args)
+
+    print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
+    print("{}".format(args).replace(', ', ',\n'))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    cudnn.benchmark = True
+
+    tf_writer = SummaryWriter(log_dir=os.path.join(args.log_dir, "log"))
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    train_dataset = ImbalanceCIFAR10(
+            root=args.data_path, train=True, download=True, transform=transform_train
+        )
+    
+    num_per_class = torch.tensor(train_dataset.get_cls_num_list())
+    val_dataset = datasets.CIFAR10(root=args.data_path,
+                                       train=False, download=True, transform=transform_val)
+
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, 
+                                               shuffle=True, num_workers=args.num_workers, 
+                                               pin_memory=args.pin_mem)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=100, shuffle=False,
+                                            num_workers=args.num_workers, pin_memory=True)
+
+    
+    # define the model
+    model = models_vit.__dict__[args.model](num_classes=10)
+
+    if args.start_epoch == 0:
+        checkpoint = torch.load(args.resume, map_location='cpu')
+        checkpoint_model = checkpoint['model']
+        state_dict = model.state_dict()
+        for k in ['head.weight', 'head.bias']:
+            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
+                print(f"Removing key {k} from pretrained checkpoint")
+                del checkpoint_model[k]
+        # interpolate position embedding
+        interpolate_pos_embed(model, checkpoint_model)
+        # load pre-trained model
+        msg = model.load_state_dict(checkpoint_model, strict=False)
+        assert set(msg.missing_keys) == {'head.weight', 'head.bias'}
+
+        # for linear prob only
+        # hack: revise model's head with BN
+        trunc_normal_(model.head.weight, std=0.01)
+        model.head = torch.nn.Sequential(torch.nn.BatchNorm1d(model.head.in_features, affine=False, eps=1e-6), model.head)
+    else:
+        model = torch.load(f"./classify_output_dir/epoch-{args.start_epoch}")
+    
+    
+    # freeze all but the head
+    for _, p in model.named_parameters():
+        p.requires_grad = False
+    for _, p in model.head.named_parameters():
+        p.requires_grad = True
+    model.to(device)
+
+    args.lr = args.blr * args.batch_size / 256
+
+    optimizer = LARS(model.head.parameters(), lr=args.lr, weight_decay=0)
+
+
+    beta = 0.9999
+    effective_num = 1.0 - np.power(beta, num_per_class)
+    per_cls_weights = (1.0 - beta) / np.array(effective_num)
+    per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(num_per_class)
+    per_cls_weights = torch.FloatTensor(per_cls_weights).to(device)
+    losser = torch.nn.CrossEntropyLoss(per_cls_weights)
+
+    
+
+    print(f"Start training for {args.epochs} epochs")
+    for epoch in range(args.start_epoch, args.epochs):
+        losses = AverageMeter('Loss', ':.4e')
+        top1 = AverageMeter('Acc@1', ':6.2f')
+        top3 = AverageMeter('Acc@3', ':6.2f')
+        model.train()
+        for i, (img, label) in enumerate(train_loader):
+            img = img.to(device)
+            label = label.to(device)
+            
+            lr_sched.adjust_learning_rate(optimizer, i / len(train_loader) + epoch, args)
+            # adjust_learning_rate(optimizer, epoch, args)
+            with torch.cuda.amp.autocast():
+                pred = model(img)
+                loss = losser(pred, label)
+            
+            acc1, acc3 = accuracy(pred, label, topk=(1, 3))
+            losses.update(loss.item(), img.size(0))
+            top1.update(acc1[0], img.size(0))
+            top3.update(acc3[0], img.size(0))         
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if i % 10 == 0:
+                output = ('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
+                          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                          'Prec@3 {top3.val:.3f} ({top3.avg:.3f})'.format(
+                    epoch, i, len(train_loader), loss=losses, top1=top1, top3=top3,
+                    lr=optimizer.param_groups[-1]['lr']))
+                print(output)
+            
+        tf_writer.add_scalar('loss/train', losses.avg, epoch)
+        tf_writer.add_scalar('acc/train_top1', top1.avg, epoch)
+        tf_writer.add_scalar('acc/train_top3', top3.avg, epoch)
+        tf_writer.add_scalar('lr', optimizer.param_groups[-1]['lr'], epoch)
+        
+            
+        if args.output_dir and (epoch % 5 == 0 or epoch + 1 == args.epochs):
+            validate(val_loader, model, losser, epoch, tf_writer)
+            torch.save(model, f"./classify_head/epoch-{epoch}")
+
+
+
+def validate(val_loader, model, criterion, epoch, tf_writer, flag='val'):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top3 = AverageMeter('Acc@3', ':6.2f')
+
+    # switch to evaluate mode
+    model.eval()
+    all_preds = []
+    all_targets = []
+    with torch.no_grad():
+        end = time.time()
+        for i, (img, label) in enumerate(val_loader):
+            img = img.cuda()
+            label = label.cuda()
+
+            with torch.cuda.amp.autocast():
+                pred = model(img)
+                loss = criterion(pred, label)
+
+            acc1, acc3 = accuracy(pred, label, topk=(1, 3))
+            losses.update(loss.item(), img.size(0))
+            top1.update(acc1[0], img.size(0))
+            top3.update(acc3[0], img.size(0))
+
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            _, pred = torch.max(pred, 1)
+            all_preds.extend(pred.cpu().numpy())
+            all_targets.extend(label.cpu().numpy())
+
+            if i % 20 == 0:
+                output = ('Test: [{0}/{1}]\t'
+                          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                          'Prec@3 {top3.val:.3f} ({top3.avg:.3f})'.format(
+                    i, len(val_loader), batch_time=batch_time, loss=losses,
+                    top1=top1, top3=top3))
+                print(output)
+        cf = confusion_matrix(all_targets, all_preds).astype(float)
+        cls_cnt = cf.sum(axis=1)
+        cls_hit = np.diag(cf)
+        cls_acc = cls_hit / cls_cnt
+        output = ('{flag} Results: Prec@1 {top1.avg:.3f} Prec@3 {top3.avg:.3f} Loss {loss.avg:.5f}'
+                  .format(flag=flag, top1=top1, top3=top3, loss=losses))
+        out_cls_acc = '%s Class Accuracy: %s' % (
+            flag, (np.array2string(cls_acc, separator=',', formatter={'float_kind': lambda x: "%.3f" % x})))
+        print(output)
+        print(out_cls_acc)
+
+
+        if tf_writer is not None:
+            tf_writer.add_scalar('loss/test_' + flag, losses.avg, epoch)
+            tf_writer.add_scalar('acc/test_' + flag + '_top1', top1.avg, epoch)
+            tf_writer.add_scalar('acc/test_' + flag + '_top3', top3.avg, epoch)
+            tf_writer.add_scalars('acc/test_' + flag + '_cls_acc', {str(i): x for i, x in enumerate(cls_acc)}, epoch)
+
+    return top1.avg 
+
+
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
---
+++ train_ssp.py
@@ -0,0 +1,308 @@
+import argparse
+import random
+import time
+import warnings
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import models.resnet_cifar as models
+from tensorboardX import SummaryWriter
+from utils import *
+from dataset.imbalance_cifar import ImbalanceCIFAR10
+from utils.losses import LDAMLoss, FocalLoss
+
+model_names = sorted(name for name in models.__dict__
+                     if name.islower() and not name.startswith("__")
+                     and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', type=str, default='./data')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet32', choices=model_names,
+                    help='model architecture: ' + ' | '.join(model_names))
+parser.add_argument('--loss_type', default="CE", type=str, choices=['CE', 'Focal', 'LDAM'])
+parser.add_argument('--imb_type', default="exp", type=str, help='imbalance type')
+parser.add_argument('--imb_factor', default=0.02, type=float, help='imbalance factor')
+parser.add_argument('--train_rule', default='Reweight', type=str,
+                    choices=['None', 'Resample', 'Reweight', 'DRW'])
+parser.add_argument('--rand_number', default=0, type=int, help='fix random number for data sampling')
+parser.add_argument('--exp_str', default='ss_pretrained', type=str,
+                    help='(additional) name to indicate experiment')
+parser.add_argument('--gpu', default=0, type=int, help='GPU id to use')
+parser.add_argument('--pretrained_model', type=str, default='checkpoint/cifar10_resnet32_CE_None_exp_0.02_pretrain_rot/ckpt.best.pth.tar')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N')
+parser.add_argument('--epochs', default=200, type=int, metavar='N')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N')
+parser.add_argument('-b', '--batch-size', default=128, type=int, metavar='N')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, metavar='LR', dest='lr')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M')
+parser.add_argument('--wd', '--weight-decay', default=2e-4, type=float, metavar='W', dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=10, type=int, metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true', help='evaluate model on validation set')
+parser.add_argument('--seed', default=None, type=int, help='seed for initializing training.')
+parser.add_argument('--root_log', type=str, default='log')
+parser.add_argument('--root_model', type=str, default='./checkpoint')
+best_acc1 = 0
+
+
+def main():
+    args = parser.parse_args()
+    args.store_name = '_'.join(["cifar10", args.arch, args.loss_type, args.train_rule,
+                                args.imb_type, str(args.imb_factor), args.exp_str])
+    prepare_folders(args)
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        
+    main_worker(args.gpu, args)
+
+
+def main_worker(gpu, args):
+    global best_acc1
+    args.gpu = gpu
+    torch.cuda.set_device(args.gpu)
+
+    if args.gpu is not None:
+        print(f"Use GPU: {args.gpu} for training")
+
+    print(f"===> Creating model '{args.arch}'")
+    num_classes = 10
+    use_norm = True if args.loss_type == 'LDAM' else False
+    model = models.__dict__[args.arch](num_classes=num_classes, use_norm=use_norm)
+    model = model.cuda()
+
+
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum, weight_decay=args.weight_decay)
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    transform_val = transforms.Compose([
+        transforms.ToTensor(),   
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    train_dataset = ImbalanceCIFAR10(
+        root=args.data_path, imb_type=args.imb_type, imb_factor=args.imb_factor,
+        rand_number=args.rand_number, train=True, download=True, transform=transform_train
+    )
+    val_dataset = datasets.CIFAR10(root=args.data_path,
+                                   train=False, download=True, transform=transform_val)
+
+    train_loader = torch.utils.data.DataLoader( train_dataset, batch_size=args.batch_size, 
+                                               shuffle=True, num_workers=args.workers, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=100, shuffle=False,
+                                                 num_workers=args.workers, pin_memory=True)
+
+
+    # evaluate only
+    if args.evaluate:
+        assert args.resume, 'Specify a trained model using [args.resume]'
+        checkpoint = torch.load(args.resume, map_location=torch.device(f'cuda:{str(args.gpu)}'))
+        model.load_state_dict(checkpoint['state_dict'])
+        print(f"===> Checkpoint '{args.resume}' loaded, testing...")
+        validate(val_loader, model, nn.CrossEntropyLoss(), 0, args)
+        return
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print(f"===> Loading checkpoint '{args.resume}'")
+            checkpoint = torch.load(args.resume, map_location=torch.device(f'cuda:{str(args.gpu)}'))
+            args.start_epoch = checkpoint['epoch']
+            best_acc1 = checkpoint['best_acc1']
+            if args.gpu is not None:
+                best_acc1 = best_acc1.to(args.gpu)
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print(f"===> Loaded checkpoint '{args.resume}' (epoch {checkpoint['epoch']})")
+        else:
+            raise ValueError(f"No checkpoint found at '{args.resume}'")
+
+    # load self-supervised pre-trained model
+    if args.pretrained_model:
+        checkpoint = torch.load(args.pretrained_model, map_location=torch.device(f'cuda:{str(args.gpu)}'))
+        if 'moco_ckpt' not in args.pretrained_model:
+            from collections import OrderedDict
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint['state_dict'].items():
+                if 'linear' not in k and 'fc' not in k:
+                    new_state_dict[k] = v
+            model.load_state_dict(new_state_dict, strict=False)
+            print(f'===> Pretrained weights found in total: [{len(list(new_state_dict.keys()))}]')
+        print(f'===> Pre-trained model loaded: {args.pretrained_model}')
+
+    cudnn.benchmark = True
+
+    cls_num_list = train_dataset.get_cls_num_list()
+    print('cls num list:')
+    print(cls_num_list)
+    args.cls_num_list = cls_num_list
+
+    tf_writer = SummaryWriter(log_dir=os.path.join(args.root_log, args.store_name))
+
+    for epoch in range(args.start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch, args)
+
+        if args.train_rule == 'Reweight':
+            beta = 0.9999
+            effective_num = 1.0 - np.power(beta, cls_num_list)
+            per_cls_weights = (1.0 - beta) / np.array(effective_num)
+            per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+            per_cls_weights = torch.FloatTensor(per_cls_weights).cuda(args.gpu)
+        elif args.train_rule == 'DRW':
+            idx = epoch // 160
+            betas = [0, 0.9999]
+            effective_num = 1.0 - np.power(betas[idx], cls_num_list)
+            per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+            per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+            per_cls_weights = torch.FloatTensor(per_cls_weights).cuda(args.gpu)
+        else:
+            per_cls_weights = None
+
+        if args.loss_type == 'CE':
+            criterion = nn.CrossEntropyLoss(weight=per_cls_weights).cuda(args.gpu)
+        elif args.loss_type == 'LDAM':
+            criterion = LDAMLoss(cls_num_list=cls_num_list, max_m=0.5, s=30, weight=per_cls_weights).cuda(args.gpu)
+        elif args.loss_type == 'Focal':
+            criterion = FocalLoss(weight=per_cls_weights, gamma=1).cuda(args.gpu)
+        else:
+            warnings.warn('Loss type is not listed')
+            return
+
+        train(train_loader, model, criterion, optimizer, epoch, args, tf_writer)
+        acc1 = validate(val_loader, model, criterion, epoch, args, tf_writer)
+
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        tf_writer.add_scalar('acc/test_top1_best', best_acc1, epoch)
+        output_best = 'Best Prec@1: %.3f\n' % best_acc1
+        print(output_best)
+
+        save_checkpoint(args, {
+            'epoch': epoch + 1,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'best_acc1': best_acc1,
+            'optimizer': optimizer.state_dict(),
+        }, is_best)
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args, tf_writer):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+
+    model.train()
+
+    end = time.time()
+    for i, (inputs, target) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        inputs = inputs.cuda()
+        target = target.cuda()
+        output = model(inputs)
+        loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), inputs.size(0))
+        top1.update(acc1[0], inputs.size(0))
+        top5.update(acc5[0], inputs.size(0))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            output = ('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                epoch, i, len(train_loader), batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'] * 0.1))
+            print(output)
+
+    tf_writer.add_scalar('loss/train', losses.avg, epoch)
+    tf_writer.add_scalar('acc/train_top1', top1.avg, epoch)
+    tf_writer.add_scalar('acc/train_top5', top5.avg, epoch)
+    tf_writer.add_scalar('lr', optimizer.param_groups[-1]['lr'], epoch)
+
+
+def validate(val_loader, model, criterion, epoch, args, tf_writer=None, flag='val'):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+
+    # switch to evaluate mode
+    model.eval()
+    all_preds = []
+    all_targets = []
+    with torch.no_grad():
+        end = time.time()
+        for i, (inputs, target) in enumerate(val_loader):
+            inputs = inputs.cuda()
+            target = target.cuda()
+
+            output = model(inputs)
+            loss = criterion(output, target)
+
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(acc1[0], inputs.size(0))
+            top5.update(acc5[0], inputs.size(0))
+
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            _, pred = torch.max(output, 1)
+            all_preds.extend(pred.cpu().numpy())
+            all_targets.extend(target.cpu().numpy())
+
+            if i % args.print_freq == 0:
+                output = ('Test: [{0}/{1}]\t'
+                          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                          'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                    i, len(val_loader), batch_time=batch_time, loss=losses,
+                    top1=top1, top5=top5))
+                print(output)
+        cf = confusion_matrix(all_targets, all_preds).astype(float)
+        cls_cnt = cf.sum(axis=1)
+        cls_hit = np.diag(cf)
+        cls_acc = cls_hit / cls_cnt
+        output = ('{flag} Results: Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Loss {loss.avg:.5f}'
+                  .format(flag=flag, top1=top1, top5=top5, loss=losses))
+        out_cls_acc = '%s Class Accuracy: %s' % (
+            flag, (np.array2string(cls_acc, separator=',', formatter={'float_kind': lambda x: "%.3f" % x})))
+        print(output)
+        print(out_cls_acc)
+
+        if tf_writer is not None:
+            tf_writer.add_scalar('loss/test_' + flag, losses.avg, epoch)
+            tf_writer.add_scalar('acc/test_' + flag + '_top1', top1.avg, epoch)
+            tf_writer.add_scalar('acc/test_' + flag + '_top5', top5.avg, epoch)
+            tf_writer.add_scalars('acc/test_' + flag + '_cls_acc', {str(i): x for i, x in enumerate(cls_acc)}, epoch)
+
+    return top1.avg
+
+
+if __name__ == '__main__':
+    main()
